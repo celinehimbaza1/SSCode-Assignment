10fc40e8f92b8e57cff9512a40844af00813a343
celine <celine.himbaza@gmail.com>
2025-03-28 12:50:50 +0200

    First assignment:Carrying out operations in arrays

---
+++ Assignment1/array.js
@@ -0,0 +1,28 @@
+const array = [
+    {name:'Laptop',price:800, category:'Electronics'},
+    {name:'Phone',price:500,category:'Electronics'},
+    {name:'Table',price:200,category:'Furniture'},
+    {name:'chair',price:100,category:'Furniture'},
+    {name:'Headfone',price:50,category:'Electronics'},
+    { name: "Shoes", price: 70, category: "Clothing" },
+    { name: "Jacket", price: 120, category: "Clothing" },
+    { name: "Watch", price: 250, category: "Accessories" },
+    { name: "Backpack", price: 90, category: "Accessories" },
+    { name: "Sofa", price: 600, category: "Furniture" }
+]
+
+console.log(array)
+
+//Part1 Javascript operation
+ // using push() array method to add a new product
+
+ const addProduct= {name:'Desk',price:150,category:'Furniture'}
+  array.push(addProduct)
+  console.log(array)
+// array.push({name:'Desk',price:150,category:'Furniture'}) this also can work
+
+//Using find() method to get the details of the product"Watch" and display them
+  
+
+
+
