571ecf7c88b0b1bde7648a1f1ce9542b16a56ff5
celine <celine.himbaza@gmail.com>
2025-04-05 06:36:45 +0200

    Tailwind Portfolio

---
+++ Tailwind CSS/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",  // Add paths to all your files
+    "./path/to/your/files/**/*.{html,js}", // Example
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
